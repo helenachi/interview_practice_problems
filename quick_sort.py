abfd68c56e6b89b1d44be5acc3b91267c21d1202
Helena Chi <hchi124@gmail.com>
2020-06-13 17:36:13 -0500

    added quickselect to quicksort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,6 +1,3 @@
-import random
-
-
 def swap(array, i, j):
   # array[i], array[j] = array[j], array[i]
   temp = array[i]
@@ -26,6 +23,24 @@ def quicksort(array, lo, hi):
   return array
 
 
+def quickselect(array, k):
+  if not array or len(array) == 0:
+    return None
+  elif len(array) == 1:
+    return array[0]
+  else:
+    index = partition(array, 0, len(array)-1)
+    if k == index:
+      return array[index]
+    elif k < index:
+      if (k < 0):
+        k = 0
+      return quickselect(array[:index], k)
+    else:
+      return quickselect(array[index + 1:], k - index)
+
 array = [4, 0, 2, -1, 3]
+array_cp = [4, 0, 2, -1, 3]
 print(array)
 print(quicksort(array, 0, len(array) - 1))
+print(quickselect(array_cp, -2))
